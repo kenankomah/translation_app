cf32948dfe4bdd1f5de91b3bd4a3cc4e4daa0acd
Kenneth Ankomah <kenankomah@live.co.uk>
2024-03-24 16:28:59 +0000

    turned app into a translator

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -4,33 +4,33 @@ import { OpenAIStream, StreamingTextResponse } from "ai";
 export const runtime = "edge"; // provide optimal infrastructure for our API route (https://edge-runtime.vercel.app/)
 
 const config = new Configuration({
-    apiKey: process.env.OPENAI_API_KEY,
+	apiKey: process.env.OPENAI_API_KEY,
 });
 
 const openai = new OpenAIApi(config);
 
 export async function POST(request: Request) {
-    const { messages } = await request.json();
-    console.log(messages, "messages");
+	const { messages } = await request.json();
+	console.log(messages, "messages");
 
-    //createChatCompletion (get response from OpenAI)
-    const response = await openai.createChatCompletion({
-        // model: "gpt-4",
-        model: "gpt-4",
-        stream: true,
-        messages: [
-            {
-                role: "system",
-                content:
-                    "You are a helpful assistant. You explain software concepts simply to intermediate programmers.",
-            },
-            ...messages,
-        ],
-    });
+	//createChatCompletion (get response from OpenAI)
+	const response = await openai.createChatCompletion({
+		// model: "gpt-4",
+		model: "gpt-4",
+		stream: true,
+		messages: [
+			{
+				role: "system",
+				content:
+					"You are a translator. You translate from English to German.",
+			},
+			...messages,
+		],
+	});
 
-    // create a stream to get the response from OpenAI (stream data to the frontend)
-    const stream = await OpenAIStream(response);
+	// create a stream to get the response from OpenAI (stream data to the frontend)
+	const stream = await OpenAIStream(response);
 
-    //send the response to the client/frontend
-    return new StreamingTextResponse(stream);
+	//send the response to the client/frontend
+	return new StreamingTextResponse(stream);
 }
--- components/chatComponent.tsx
+++ components/chatComponent.tsx
@@ -2,48 +2,47 @@
 import { useChat, Message } from "ai/react";
 
 export default function ChatComponent() {
-    const { input, handleInputChange, handleSubmit, isLoading, messages } =
-        useChat();
-    // console.log("OPENAI_API_KEY", process);
-    console.log(messages);
-    return (
-        <div>
-            {messages.map((message: Message) => {
-                return (
-                    <div key={message.id}>
-                        {message.role === "assistant" ? (
-                            <h3 className="mt-2 text-lg font-semibold">
-                                GPT-4
-                            </h3>
-                        ) : (
-                            <h3 className="mt-2 text-lg font-semibold">User</h3>
-                        )}
-                        {message.content
-                            .split("\n")
-                            .map((line: string, i: number) => {
-                                if (line === "") {
-                                    return <p key={message.id + i}>&nbsp;</p>;
-                                }
+	const { input, handleInputChange, handleSubmit, isLoading, messages } =
+		useChat();
+	// console.log("OPENAI_API_KEY", process);
+	console.log(messages);
+	return (
+		<div>
+			{messages.length !== 0 &&
+				messages[0].content
+					.split("\n")
+					.map((line: string, i: number) => {
+						if (line === "") {
+							return <p key={messages[0].id + i}>&nbsp;</p>;
+						}
 
-                                return <p key={message.id + i}>{line}</p>;
-                            })}
-                    </div>
-                );
-            })}
+						return <p key={messages[0].id + i}>{line}</p>;
+					})}
 
-            <form className="mt-12" onSubmit={handleSubmit}>
-                <p>User Message</p>
-                <textarea
-                    className="mt-2 w-full bg-slate-600 p-2"
-                    placeholder={"What are data structures and algorithms?"}
-                    value={input}
-                    onChange={handleInputChange}
-                ></textarea>
+			{messages.length !== 0 &&
+				messages[1]?.content
+					.split("\n")
+					.map((line: string, i: number) => {
+						if (line === "") {
+							return <p key={messages[0].id + i}>&nbsp;</p>;
+						}
 
-                <button className="mt-2 rounded-md bg-blue-600 p-2">
-                    Send Message
-                </button>
-            </form>
-        </div>
-    );
+						return <p key={messages[0].id + i}>{line}</p>;
+					})}
+
+			<form className="mt-12" onSubmit={handleSubmit}>
+				<p>User Message</p>
+				<textarea
+					className="mt-2 w-full bg-slate-600 p-2"
+					placeholder={"What are data structures and algorithms?"}
+					value={input}
+					onChange={handleInputChange}
+				></textarea>
+
+				<button className="mt-2 rounded-md bg-blue-600 p-2">
+					Send Message
+				</button>
+			</form>
+		</div>
+	);
 }
